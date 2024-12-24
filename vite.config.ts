20624e0e1f0866a6bf731f0124f299bac7b64e7c
sinsky <60662802+sinsky@users.noreply.github.com>
2024-12-25 01:48:51 +0900

    chore(vite.config.ts): add async script option to ssgOptions for improved performance during static site generation

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import VueRouter from "unplugin-vue-router/vite";
+import type { ViteSSGOptions } from "vite-ssg";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), VueRouter()],
+  ssgOptions: {
+    script: "async",
+  } as ViteSSGOptions,
 });
