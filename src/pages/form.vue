7a5ca612e71d56765f84c4281f841c8c2985b736
sinsky <60662802+sinsky@users.noreply.github.com>
2024-12-25 02:00:18 +0900

    feat(form.vue): implement form submission handling with fetch API to send data to the server and navigate to thank-you page upon success

---
+++ src/pages/form.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { useHead } from "@unhead/vue";
+
+useHead({
+  title: "form",
+  meta: [
+    {
+      name: "description",
+      content: "Please cooperate with the survey.",
+    },
+  ],
+});
+
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const handleSubmit = (event: Event) => {
+  event.preventDefault();
+
+  const myForm = event.target as HTMLFormElement;
+  const formData = new FormData(myForm) as any;
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => router.push("/thank-you/"))
+    .catch((error) => alert(error));
+};
+</script>
+
+<template>
+  <h1>Survey</h1>
+  <form
+    name="demo survey"
+    method="POST"
+    style="display: flex; flex-direction: column; gap: 2rem"
+    netlify
+    @submit="handleSubmit"
+  >
+    <div class="form-col">
+      <label for="name">Name</label>
+      <input type="text" name="name" id="name" />
+    </div>
+    <div class="form-col">
+      <label for="message">Message</label>
+      <textarea name="message" id="message" cols="30" rows="10"></textarea>
+    </div>
+    <div class="form-last"><button type="submit">Submit</button></div>
+  </form>
+</template>
+
+<style>
+.form-col {
+  gap: 0.5rem;
+  display: grid;
+  grid-template-columns: 1fr 4fr;
+}
+.form-last {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
